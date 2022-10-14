f54273b56f39a9007735f731208638db97155341
Misael Vilca <mrevilca31@gmail.com>
2022-10-13 23:32:39 -0300

    Modificaciones en crear tarjeta

--- src/routes/Card/CrearCard.jsx
+++ src/routes/Card/CrearCard.jsx
@@ -1,12 +1,14 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TarjetasDeClimaContext } from "../../context/TarjetasDeClimaContext";
 import { useForm } from "react-hook-form";
+import { getCardInfo } from "../../service";
 
 const CrearCard = () => {
   const { tarjetasDeClima, setTarjetasDeClima } = useContext(
     TarjetasDeClimaContext
   );
+  const [cardInfo, setCardInfo] = useState([]);
   const navigate = useNavigate();
 
   const {
@@ -15,15 +17,25 @@ const CrearCard = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    //console.log(data);
+
+    // getCardInfo(data.latitud, data.longitud)
+    //   .then((data) => setCardInfo(data))
+    //   .catch((err) => console.log(err));
+
     const nuevaCard = {
       id: tarjetasDeClima.length + 1,
       name: data.cardName,
       latitud: data.latitud,
       longitud: data.longitud,
+      // temperatura: cardInfo.current_weather.temperature,
+      // velocidad: cardInfo.current_weather.windspeed,
     };
+    console.log(nuevaCard);
+
     setTarjetasDeClima([...tarjetasDeClima, nuevaCard]);
+    console.log(tarjetasDeClima);
     navigate("/");
   };
 
@@ -44,22 +56,22 @@ const CrearCard = () => {
         <input
           className="input-card-longitud-form"
           type="text"
-          placeholder="Longitud"
-          {...register("longitud", {
-            required: "Debe ingresar un valor de longitud",
-          })}
-        />
-        <p>{errors.longitud?.message}</p>
-
-        <input
-          className="input-card-latitud-form"
-          type="text"
           placeholder="Latitud"
           {...register("latitud", {
             required: "Debe ingresar un valor de latitud",
           })}
         />
         <p>{errors.latitud?.message}</p>
+
+        <input
+          className="input-card-latitud-form"
+          type="text"
+          placeholder="Longitud"
+          {...register("longitud", {
+            required: "Debe ingresar un valor de longitud",
+          })}
+        />
+        <p>{errors.longitud?.message}</p>
 
         <button className="btn-form" type="submit">
           Crear Tarjeta
