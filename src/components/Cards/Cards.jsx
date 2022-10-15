54ac08aafc61589229df2febce24ef1147e0afd9
Cecilia <cecilia.gimena.hidalgo@gmail.com>
2022-10-15 03:48:03 -0300

    new

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,12 +3,15 @@ import Card from "./Card";
 // se necesita recibir un listado de tarjetas
 const Cards = ({ cards }) => {
   return (
-    <div className="">
+    <div>
       <h2>Lista de Tarjetas</h2>
-      {cards.map((card) => (
+      <div className="nueva_clase">
+        {cards.map((card) => (
         // falta asignar un id para las tarjetas
         <Card key={card.id} cardName={card.name} id={card.id} />
       ))}
+      </div>
+      
     </div>
   );
 };
